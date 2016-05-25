ee47516cc16e515aa2bb2eebb92a754144c2b905
Everest Witman <etwitman@gmail.com>
2016-05-25 10:16:49 -0400

    Second commit

---
+++ app/assets/javascripts/play-card.js
@@ -0,0 +1,14 @@
+//Allows the user to click a card and then have it
+//be played on table and removed from their hand. 
+
+$(document).ready(function(event){
+	$(document).on('click', '.hand .card', function(event){
+   if ($(this).css('background') !== $('.table .card').css('background')){
+    $('.table .card').css('background', $(this).css('background'));
+    $(this).remove();
+   }
+   else{
+    alert("This is not a legal play. You can only play a card of a different color from the one on the table.");
+   }
+	});
+});
